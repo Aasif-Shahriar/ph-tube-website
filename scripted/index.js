e7ae7bc2d8c1670abdbf3769ee40ca553470adee
Aasif-Shahriar <asifshahriar247@gmail.com>
2025-03-12 22:50:29 +0600

    added dynamic buttons

--- scripted/index.js
+++ scripted/index.js
@@ -1,3 +1,10 @@
+function removeClass () {
+    const activeBtns = document.getElementsByClassName("active")
+    for(let btn of activeBtns){
+        btn.classList.remove("active");
+    }
+}
+
 function loadCategories(){
     //fetching data from the api
     fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
@@ -9,31 +16,37 @@ function loadVideos(){
     //fetching data from the api
     fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
         .then((res)=> res.json())
-        .then((data) => displayVideo(data.videos))//Sending data to this function
+        .then((data) => {
+            document.getElementById("btn-all").classList.add("active")
+            displayVideo(data.videos)
+        })//Sending data to this function
 }
 
-
-// {
-//     "category_id": "1001",
-//     "video_id": "aaaa",
-//     "thumbnail": "https://i.ibb.co/L1b6xSq/shape.jpg",
-//     "title": "Shape of You",
-//     "authors": [
-//         {
-//             "profile_picture": "https://i.ibb.co/D9wWRM6/olivia.jpg",
-//             "profile_name": "Olivia Mitchell",
-//             "verified": ""
-//         }
-//     ],
-//     "others": {
-//         "views": "100K",
-//         "posted_date": "16278"
-//     },
-//     "description": "Dive into the rhythm of 'Shape of You,' a captivating track that blends pop sensibilities with vibrant beats. Created by Olivia Mitchell, this song has already gained 100K views since its release. With its infectious melody and heartfelt lyrics, 'Shape of You' is perfect for fans looking for an uplifting musical experience. Let the music take over as Olivia's vocal prowess and unique style create a memorable listening journey."
-// }
+const loadCategoryVideos = (id) => {
+    const url = `https://openapi.programming-hero.com/api/phero-tube/category/${id}`
+    fetch(url)
+    .then(res => res.json())
+    .then(data => {
+        removeClass();
+        const clickedBtn = document.getElementById(`btn-${id}`);
+        clickedBtn.classList.add("active");
+        displayVideo(data.category);
+    })
+}
 
 const displayVideo = (videos) => {
    const videoContainer = document.getElementById('video-container');
+
+   videoContainer.innerHTML = "";
+
+   if(videos.length == 0){
+    videoContainer.innerHTML = ` <div class="col-span-full text-center py-20">
+        <img class="w-28 mx-auto" src="assets/Icon.png" alt="">
+        <h2 class="text-2xl font-bold">Oops!! Sorry, There is no content here</h2>
+    </div>`;
+    return;
+   }
+
    videos.forEach(video => {
     const videoCard = document.createElement('div')
     videoCard.innerHTML = `
@@ -67,12 +80,6 @@ const displayVideo = (videos) => {
    });
 }
 
-
-// {
-//     "category_id": "1001",
-//     "category": "Music"
-// }
-
 //Receiving data to this function
 function displayCategory(categories){
     // getting the category container
@@ -84,7 +91,7 @@ function displayCategory(categories){
        const div = document.createElement("div");
        //dynamically adding the category to the div content
        div.innerHTML = `
-         <button class="btn btn-sm hover:bg-red-500 hover:text-white">${cat.category}</button>
+         <button id="btn-${cat.category_id}" onclick="loadCategoryVideos(${cat.category_id})" class="btn btn-sm hover:bg-red-500 hover:text-white">${cat.category}</button>
        `;
        //appending the div to the category container
        categoryContainer.appendChild(div);
@@ -94,4 +101,28 @@ function displayCategory(categories){
 }
 
 loadCategories();
-loadVideos(); 
+
+// {
+//     "category_id": "1001",
+//     "video_id": "aaaa",
+//     "thumbnail": "https://i.ibb.co/L1b6xSq/shape.jpg",
+//     "title": "Shape of You",
+//     "authors": [
+//         {
+//             "profile_picture": "https://i.ibb.co/D9wWRM6/olivia.jpg",
+//             "profile_name": "Olivia Mitchell",
+//             "verified": ""
+//         }
+//     ],
+//     "others": {
+//         "views": "100K",
+//         "posted_date": "16278"
+//     },
+//     "description": "Dive into the rhythm of 'Shape of You,' a captivating track that blends pop sensibilities with vibrant beats. Created by Olivia Mitchell, this song has already gained 100K views since its release. With its infectious melody and heartfelt lyrics, 'Shape of You' is perfect for fans looking for an uplifting musical experience. Let the music take over as Olivia's vocal prowess and unique style create a memorable listening journey."
+// }
+
+
+// {
+//     "category_id": "1001",
+//     "category": "Music"
+//   }
