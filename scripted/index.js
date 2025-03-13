836270839d0d65d3a9cf5b13b321a1a848a75184
Aasif-Shahriar <asifshahriar247@gmail.com>
2025-03-13 21:10:42 +0600

    added search input

--- scripted/index.js
+++ scripted/index.js
@@ -12,9 +12,9 @@ function loadCategories(){
         .then((data) => displayCategory(data.categories))//Sending data to this function
 }
 
-function loadVideos(){
+function loadVideos(searchInput = ""){
     //fetching data from the api
-    fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
+    fetch(`https://openapi.programming-hero.com/api/phero-tube/videos?title=${searchInput}`)
         .then((res)=> res.json())
         .then((data) => {
             document.getElementById("btn-all").classList.add("active")
@@ -130,29 +130,8 @@ function displayCategory(categories){
     
 }
 
+document.getElementById('search-input').addEventListener("keyup", (e) => {
+   const input = e.target.value;
+   loadVideos(input)
+})
 loadCategories();
-
-// {
-//     "category_id": "1001",
-//     "video_id": "aaaa",
-//     "thumbnail": "https://i.ibb.co/L1b6xSq/shape.jpg",
-//     "title": "Shape of You",
-//     "authors": [
-//         {
-//             "profile_picture": "https://i.ibb.co/D9wWRM6/olivia.jpg",
-//             "profile_name": "Olivia Mitchell",
-//             "verified": ""
-//         }
-//     ],
-//     "others": {
-//         "views": "100K",
-//         "posted_date": "16278"
-//     },
-//     "description": "Dive into the rhythm of 'Shape of You,' a captivating track that blends pop sensibilities with vibrant beats. Created by Olivia Mitchell, this song has already gained 100K views since its release. With its infectious melody and heartfelt lyrics, 'Shape of You' is perfect for fans looking for an uplifting musical experience. Let the music take over as Olivia's vocal prowess and unique style create a memorable listening journey."
-// }
-
-
-// {
-//     "category_id": "1001",
-//     "category": "Music"
-//   }
